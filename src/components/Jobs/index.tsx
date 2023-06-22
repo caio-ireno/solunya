30f43b2eb7a4db5cc7e12a0d437680bfacafc687
caio-ireno <caio_ireno@hotmail.com>
2023-06-22 16:57:00 -0300

    fix: console.log

--- src/components/Jobs/index.tsx
+++ src/components/Jobs/index.tsx
@@ -5,7 +5,6 @@ import "swiper/css";
 import "swiper/css/pagination";
 
 import Image from "next/image";
-import { useState } from "react";
 // import required modules
 import { Autoplay } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -15,8 +14,6 @@ import OndasDeParto from "../../../public/ondasdeparto.png";
 import Serene from "../../../public/serene.png";
 
 export default function Jobs() {
-  const [over, setOver] = useState(false);
-  console.log(over);
   return (
     <main className=" w-full lg:h-[400px] h-[200px]  sm:mt-20 mt-5 mb-10">
       <h1 className="w-full text-center md:text-5xl sm:text2-xl text-xl font-bold sm:mb-10 mb-2">
